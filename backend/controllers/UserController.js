0798258814a3e1922af1e7c55bc13fc3689770d7
Achmat Fajri <achmatfajri@gmail.com>
2024-02-09 20:39:31 +1400

    fix update

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -31,7 +31,7 @@ export const saveUser = async (req, res) => {
 export const updateUser = async (req, res) => {
     try {
         const updatedUser =  await User.updateOne({_id: req.params.id}, {$set: req.body})
-        res.status(200).json(updateUser)
+        res.status(200).json(updatedUser)
     } catch (error) {
         res.status(400).json({message: error.message})
     }
